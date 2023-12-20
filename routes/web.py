ee70978df0fbdec9d23012a58825fdaf0e924d78
Ashley Sumagaysay <ashleysumagaysay1602@gmail.com>
2023-12-20 17:46:50 +0800

    ui

--- routes/web.py
+++ routes/web.py
@@ -6,3 +6,7 @@ web = Blueprint('web', __name__)
 @web.route('/about')
 def about():
     return render_template("about.html")
+
+@web.route('/management')
+def management():
+    return render_template("management.html")

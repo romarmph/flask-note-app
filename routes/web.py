98a3fbb8b6108a2a96df5878f08222d098075d3a
Big 250 <rg156649@gmail.com>
2023-12-21 15:50:06 +0800

    Add notes crud

--- routes/web.py
+++ routes/web.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from json import dump
 from flask import Blueprint, redirect, render_template, request, session, url_for, flash
 
 from db import db
@@ -8,6 +9,14 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(), nullable=False)
+    uid = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
 
 
@@ -41,6 +50,7 @@ def login():
             return render_template("login.html")
 
         session['username'] = username
+        session['uid'] = user.id
         next_page = request.args.get('next', url_for('web.index'))
         return redirect(next_page)
 
@@ -78,4 +88,76 @@ def about():
 @web.route('/management')
 @login_required
 def management():
-    return render_template("management.html")
+    notes = []
+    results = Notes.query.all()
+
+    if results is not None:
+        for result in results:
+            notes.append({
+                'id': result.id,
+                'title': result.title,
+                'content': result.content,
+                'created_at': result.created_at
+            })
+
+    return render_template("management.html", notes=notes)
+
+
+@web.route('/add', methods=['GET', 'POST'])
+@login_required
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        uid = session['uid']
+
+        # Check if title or content are empty
+        if not title:
+            flash('Title is required.')
+            return redirect(url_for('web.management'))
+        if not content:
+            flash('Content is required.')
+            return redirect(url_for('web.management'))
+
+        note = Notes(title=title, content=content, uid=uid)
+        db.session.add(note)
+        db.session.commit()
+
+        return redirect(url_for('web.management'))
+
+    return redirect(url_for('web.management'))
+
+
+@web.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    note = Notes.query.filter_by(id=id).first()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        # Check if title or content are empty
+        if not title:
+            flash('Title is required.')
+            return render_template("edit.html", note=note)
+        if not content:
+            flash('Content is required.')
+            return render_template("edit.html", note=note)
+
+        note.title = title
+        note.content = content
+        db.session.commit()
+
+        return redirect(url_for('web.management'))
+
+    return render_template("edit.html", note=note)
+
+
+@web.route('/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete(id):
+    note = Notes.query.filter_by(id=id).first()
+    db.session.delete(note)
+    db.session.commit()
+    return redirect(url_for('web.management'))

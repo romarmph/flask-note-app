d6e4a0ee3c3ea384282e38359233e962c631cf2d
Big 250 <rg156649@gmail.com>
2023-12-21 14:16:24 +0800

    Updae UI
    Add new files and update dependencies

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
+from functools import wraps
 from flask import Flask, render_template
+from pymysql import OperationalError
+from sqlalchemy import text
 from routes.web import web
+from db import db, init_app
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/flask_notes'
+
+init_app(app)
+
+app.secret_key = "mysecretkey"
 app.register_blueprint(web)
-
-
-@app.route("/")
-@app.route("/index")
-def index():
-    return render_template("index.html")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("error/404.html")
+
+
+@app.route('/test_db')
+def test_db():
+    try:
+        result = db.session.execute(text('SELECT * FROM users')).fetchall()
+        # Fetch all rows from the result (optional)
+        return 'Success! Connected to the database.'
+    except OperationalError:
+        return 'Error! Could not connect to the database.'
 
 
 if __name__ == '__main__':
--- routes/web.py
+++ routes/web.py
@@ -1,12 +1,81 @@
-from flask import Blueprint, render_template
+from functools import wraps
+from flask import Blueprint, redirect, render_template, request, session, url_for, flash
+
+from db import db
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
 
 web = Blueprint('web', __name__)
 
 
+@web.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if username or password are empty
+        if not username:
+            flash('Username is required.')
+            return render_template("login.html")
+        if not password:
+            flash('Password is required.')
+            return render_template("login.html")
+
+        user = Users.query.filter_by(username=username).first()
+
+        # Check if username is incorrect
+        if not user:
+            flash('Incorrect Username.')
+            return render_template("login.html")
+
+        # Check if password is incorrect
+        if user.password != password:
+            flash('Incorrect Password.')
+            return render_template("login.html")
+
+        session['username'] = username
+        next_page = request.args.get('next', url_for('web.index'))
+        return redirect(next_page)
+
+    return render_template("login.html")
+
+
+@web.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('web.login'))
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session:
+            return redirect('/login')
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@web.route("/")
+@web.route("/index")
+@login_required
+def index():
+    return render_template("index.html")
+
+
 @web.route('/about')
+@login_required
 def about():
     return render_template("about.html")
 
+
 @web.route('/management')
+@login_required
 def management():
     return render_template("management.html")
